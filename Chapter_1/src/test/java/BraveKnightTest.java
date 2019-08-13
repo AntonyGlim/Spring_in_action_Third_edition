907fead8f7a2453982f130e79557bf92b7f36ae6
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-08-13 12:29:24 +0300

    test done

--- Chapter_1/src/test/java/BraveKnightTest.java
+++ Chapter_1/src/test/java/BraveKnightTest.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import static org.mockito.Mockito.*;
+import knight.BraveKnight;
+import org.junit.Test;
+import quest.Quest;
+import quest.QuestException;
 
 public class BraveKnightTest {
+    @Test
+    public void knightShouldEmbarkOnQuest() throws QuestException {
+        Quest mockQuest = mock(Quest.class); // Создание фиктивного объекта Quest
+        BraveKnight knight = new BraveKnight(mockQuest); // Внедрение
+        knight.embarkOnQuest();
+        verify(mockQuest, times(1)).embark();
+    }
 }
